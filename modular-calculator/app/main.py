58de19e459bab8e58850d8fb9a71506bc14e8fc8
Aarya Sarfare <aarya.work005@gmail.com>
2025-12-11 21:38:55 +0530

    defined the main function to execute the CLI

--- modular-calculator/app/main.py
+++ modular-calculator/app/main.py
@@ -1,6 +1,54 @@
 from calculator.add import Add
+from calculator.subtract import Subtract
+from calculator.multiply import Multiply
+from calculator.divide import Divide
 from engine.calculator_engine import CalculatorEngine
+from utils import get_and_validate_user_input as get_and_validate
 
-engine = CalculatorEngine()
-result = engine.execute_operation(Add(2,4))
-print(result)
+def main():
+    """Main function"""
+
+    engine = CalculatorEngine()
+
+    while True:
+
+        print("\n1. Addition \n2. Subtraction")
+        print("3. Multiply \n4. Divide")
+        print("Enter 0 to exit | Enter 6 to export data | Enter 7 for help.\n")
+
+        operation_input = get_and_validate.get_and_validate_input_index()
+
+        if operation_input == 0:
+            break
+
+        num_1, num_2 = get_and_validate.get_and_validate_user_input()
+
+        # Checks User input and performs the task
+        
+        if operation_input == 1:
+            result = engine.execute_operation(Add(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 2:
+            result = engine.execute_operation(Subtract(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 3:
+            result = engine.execute_operation(Multiply(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 4:
+            try:
+                result = engine.execute_operation(Divide(num_1, num_2))
+            except ZeroDivisionError:
+                print("You can't divide a number by zero!")
+                print("\n------------------------")
+                continue
+            print(f"Result: {result}")
+        
+        print("\n------------------------")
+
+    print("\n------------------------")
+
+if __name__ == '__main__':
+    main()
---
+++ modular-calculator/utils/get_and_validate_user_input.py
@@ -0,0 +1,34 @@
+
+
+def get_and_validate_user_input():
+    """Gets and Validates the input for numbers"""
+    
+    while True:
+        try:
+            num_1 = int(input("Enter first number: "))
+            num_2 = int(input("Enter second number: "))
+            break
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+    
+    return num_1, num_2
+
+
+def get_and_validate_input_index():
+    """Gets and Validates the input index"""
+
+    user_operation_choice_validation = [0,1,2,3,4,5,6,7]
+    
+    while True:
+        try:
+            user_choice = int(input("Select the operation by index: "))
+            if user_choice in user_operation_choice_validation:
+                break
+            if user_choice not in user_operation_choice_validation:
+                print("Please input either 0, 1, 2, 3, 4, 5, 6, or 7.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return user_choice
+
