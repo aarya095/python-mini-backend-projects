76f8e31d33ff656c419109edb085ae08b65b7567
Aarya Sarfare <aarya.work005@gmail.com>
2025-12-18 14:37:14 +0530

    Revert "wrapping the modular structure with Flask"
    This reverts commit 21d7334be5024a769b5d213f21211d46221a3e99.

--- modular-calculator/app/main.py
+++ modular-calculator/app/main.py
@@ -1,5 +1,5 @@
 # Third Party Modules
-from flask import Flask
+from colorama import Fore, Style
 
 # Local Modules
 from calculator.add import Add
@@ -9,29 +9,52 @@ from calculator.divide import Divide
 from engine.calculator_engine import CalculatorEngine
 from utils import get_and_validate_user_input as get_and_validate
 
-app = Flask(__name__)
-engine = CalculatorEngine()
+def main():
+    """Main function"""
 
-@app.route('/home')
-def index():
-    return "Hello World!"
+    engine = CalculatorEngine()
 
-@app.route('/add/<int:num_1>/<int:num_2>')
-def add(num_1, num_2):
-    result = engine.execute_operation(Add(num_1, num_2))
-    return str(result)
+    while True:
+        print(Fore.LIGHTCYAN_EX, Style.BRIGHT + "\n------------------------\n")
+        print("1. Addition \n2. Subtraction")
+        print("3. Multiply \n4. Divide")
+        print("Enter 0 to exit.\n")
 
-@app.route('/sub/<int:num_1>/<int:num_2>')
-def subtract(num_1, num_2):
-    result = engine.execute_operation(Subtract(num_1, num_2))
-    return str(result)
+        operation_input = get_and_validate.get_and_validate_input_index()
 
-@app.route('/mul/<int:num_1>/<int:num_2>')
-def multiply(num_1, num_2):
-    result = engine.execute_operation(Multiply(num_1, num_2))
-    return str(result)
+        if operation_input == 0:
+            break
 
-@app.route('/div/<int:num_1>/<int:num_2>')
-def divide(num_1, num_2):
-    result = engine.execute_operation(Divide(num_1, num_2))
-    return str(result)
+        num_1, num_2 = get_and_validate.get_and_validate_user_input()
+
+        # Checks User input and performs the task
+        
+        if operation_input == 1:
+            result = engine.execute_operation(Add(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 2:
+            result = engine.execute_operation(Subtract(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 3:
+            result = engine.execute_operation(Multiply(num_1, num_2))
+            print(f"Result: {result}")
+
+        elif operation_input == 4:
+            try:
+                result = engine.execute_operation(Divide(num_1, num_2))
+            except ZeroDivisionError:
+                print("You can't divide a number by zero!")
+                print("\n------------------------")
+                continue
+            print(f"Result: {result}")
+
+    print(Fore.LIGHTGREEN_EX)
+    print("╔═╗┌─┐┌─┐┬  ┬┌─┐┌─┐┌┬┐┬┌─┐┌┐┌  ╔═╗┬  ┌─┐┌─┐┌─┐┌┬┐")
+    print("╠═╣├─┘├─┘│  ││  ├─┤ │ ││ ││││  ║  │  │ │└─┐├┤  ││")
+    print("╩ ╩┴  ┴  ┴─┘┴└─┘┴ ┴ ┴ ┴└─┘┘└┘  ╚═╝┴─┘└─┘└─┘└─┘─┴┘")
+    print("\n------------------------")
+
+if __name__ == '__main__':
+    main()
